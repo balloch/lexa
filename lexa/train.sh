34ea35f87bcb78b9e871b135533a5a238e9d2189
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2022-11-09 16:33:19 -0500

    modified train script

--- lexa/train.sh
+++ lexa/train.sh
@@ -1,1 +1,1 @@
-python train.py --configs defaults lexa_temporal --task MiniGrid-DoorKey-8x8-v0 --logdir ~/logdir/lexa_minigrid
+python train.py --configs defaults --task MiniGrid-DoorKey-8x8-v0 --logdir /home/ei-lab/logdir/lexa_minigrid
